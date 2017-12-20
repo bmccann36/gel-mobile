e6debbd45878c3d2a17893189d85b7e6f5ff3c3d
Brian McCann <bmccann36@gmail.com>
2017-12-20 10:35:42 -0500

    rendering things to canvas with rn canvas example

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Test from './Test'
+import RnCanvas from './RnCanvas'
 
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <Test />
-    );
+        <RnCanvas />
+
+        );
   }
 }
 
 
-{/* <Image
-source = {require('./assets/bear.jpg')}
-/> */}
+
 
 
 
--- Test.js
+++ Test.js
@@ -1,47 +1,54 @@
 import React, { Component } from 'react';
-import Canvas from 'react-native-canvas';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { StyleSheet, View, Image } from 'react-native';
+import Canvas, { Image as CanvasImage, Path2D } from 'react-native-canvas';
 
 export default class Test extends Component {
 
-  // handleCanvas = (canvas) => {
-  //   const ctx = canvas.getContext('2d');
-  //   ctx.fillStyle = 'purple';
-  //   ctx.fillRect(0, 0, 100, 100);
-  // }
-  componentDidMount() {
-    this.updateCanavas()
+  handleCanvas(canvas) {
+    const CanvasXSize = 100;
+    const CanvasYSize = 100;
+    canvas.width = CanvasXSize;
+    canvas.height = CanvasYSize;
+    const ctx = canvas.getContext('2d');
+    const img = new CanvasImage(canvas);
+
+    img.src = 'http://www.ikea.com/gb/en/images/products/vardagen-coffee-cup-and-saucer-off-white__0417311_pe579389_s5.jpg'
+    img.addEventListener('load', () => {
+      console.log('image is loaded');
+      ctx.drawImage(img, 0, 0, 100, 100);
+      let imageData = ctx.getImageData(0, 0, 100, 100)
+      .then( data => {
+        console.log(data)
+      })
+
+    });
   }
 
-  updateCanavas() {
-    const ctx = this.refs.canvas.getContext('2d');
-    var imageObj1 = new Image();
-    imageObj1.src = require('./assets/bear.jpg')
-    const loadImg = async function (){
-
-    }
-    // imageObj1.onload = function () {
-    //   console.log('work')
-    //   ctx.drawImage(imageObj1, 0, 0);
-    // }
-
+  render() {
+    return (
+      <View style={styles.container}>
+        <Canvas ref={this.handleCanvas} />
+      </View>
+    )
   }
+}
 
-    render() {
-      return (
-        <View style={styles.container}>
-          <Canvas ref="canvas" width={300} height={300}> </Canvas>
-        </View>
-      )
-    }
-  }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+
+      // let myFirstPromise = new Promise((resolve, reject) => {
+    //   setTimeout(function(){
+    //     resolve("Success!"); // Yay! Everything went well!
+    //   }, 2500);
+    // });
+
+    // myFirstPromise.then((successMessage) => {
+    //   console.log("Yay! " + successMessage);
+    // });
